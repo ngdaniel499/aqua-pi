83234fb1d710b5974903b40009443a897eaef1bc
ngdaniel499 <ng.daniel499@gmail.com>
2025-05-30 11:09:44 +1000

    revamped reconnect script and collated log files

--- python-scripts/cron-reconnect.sh
+++ python-scripts/cron-reconnect.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-MY_PATH="`dirname \"$0\"`"              # relative
-LOG_PATH="`( cd \"$MY_PATH\" && cd .. && pwd )`/log/network.log"
-now=$(date +"%m-%d %r")
+(
+    now=$(date +"%m-%d %r")
+    wlan='wlan0'
+    pingip='google.com'
 
-# Which Interface do you want to check
-wlan='wlan0'
-# Which address do you want to ping to see if you can connect
-pingip='google.com'
+    echo "$now Checking network connection..."
 
-# Perform the network check and reset if necessary
-/bin/ping -c 2 -I $wlan $pingip > /dev/null 2> /dev/null
-if [ $? -ge 1 ] ; then
-    echo "$now Network is DOWN. Perform a reset" >> $LOG_PATH
-    /sbin/ifdown $wlan
-    sleep 5
-    /sbin/ifup --force $wlan
-else
-    echo "$now Network is UP. Just exit the program." >> $LOG_PATH
-fi
+    /bin/ping -c 2 -I "$wlan" "$pingip" > /dev/null 2>&1
+    if [ $? -ge 1 ]; then
+        echo "$now Network is DOWN. Performing reset..."
+        /sbin/ifdown "$wlan"
+        sleep 5
+        /sbin/ifup --force "$wlan"
+    else
+        echo "$now Network is UP. No action required."
+    fi
+) >> /home/pi/cronrun.log 2>&1
